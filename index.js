e500a4c217129aa2573d5af72b219ae162585345
GabrielITsMagicTest <gastrommania7771@gmail.com>
2025-11-16 12:53:13 -0300

    a

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 	chrome = require("chrome-aws-lambda")
-	puppeteer = request("puppeteer-core")
+	puppeteer = require("puppeteer-core")
 }else {
 	puppeteer = require('puppeteer');
 }
